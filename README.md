a9ed23dc7516b022bf9205d73601fdadb3d37586
haosdent <haosdent@gmail.com>
2013-05-11 22:22:07 +0800

    Add documents.

---
+++ README.md
@@ -0,0 +1,18 @@
+# comet-example
+
+This is a simple example to show how client get continuous data from server based ajax stream.
+
+## Usage
+
+You could check out the [result][1] online directly.
+
+[1]: http://haosdent.github.io/comet-example/
+
+## Additional
+
+There are many ways to continuous synchronization between browser and server. In my opinion, we have four ways to achieve this.
+
+* Ajax streaming
+* Ajax long-polling
+* IFrame streaming
+* WebSocket
--- ajax-comet.js
+++ ajax-comet.js
@@ -5,30 +5,47 @@
  * @date: 2013-05-11
  * @overview: Comet demo based ajax stream.
  */
-window.onload = function(){
-    var xhr;
-    try{
-        xhr = new XMLHttpRequest();
-    }catch(e){
-        alert('Please test this code in Chrome.');
-        return;
-    }
 
+var showSyslog = function(addPoint, syslogUrl){
+    // Create ajax request object.
+    var xhr;
+    if(window.XMLHttpRequest){
+        // For IE7+, Firefox, Chrome, Opera, Safari
+        xhr = new XMLHttpRequest();
+    }else{
+        // For IE6, IE5
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    // When the browser is not supported, the website would show warning.
+    if(!xhr){
+        alert('Sorry, you browser is not supported!');
+        return;
+    };
+
+    // Use for mark the position of present response text.
     var pos = 0;
-    var addPoint = document.getElementById('add-point');
     // Match the informations in every syslog style record.
     var regex = /(\w+ +\d+ \d+:\d+:\d+) (\S+) ([\w \]\[]+): (.+)/;
+    // Use for simplify dom operations.
     var tempNode = document.createElement('tbody');
+    // Use for show the request status.
     var tipNode = document.getElementById('tip');
 
+    // Ajax streaming callback function.
     xhr.onreadystatechange = function(){
         if(xhr.readyState === 1){
-            tipNode.innerText = 'Finish to establish the connection.';
+            tipNode.innerText = 'Establish the connection.';
         }else if(xhr.readyState >= 3){
+            // Handle request error.
+            if(xhr.status >= 400){
+                tipNode.innerText = 'Request error.';
+                return;
+            };
+
             if(xhr.readyState === 3){
-                tipNode.innerText = 'Finish load data...';
-            }else{
                 tipNode.innerText = 'Receive data.....';
+            }else{
+                tipNode.innerText = 'Finish load data.';
             };
             var resp = xhr.responseText;
             var data = resp.substring(pos).split('\n');
@@ -54,6 +71,8 @@ window.onload = function(){
             for(var i = 0; i < l; i++){
                 var d = data[i];
                 var results = regex.exec(d);
+                // Skip the record if the format of it is invaild.
+                if(results === null) continue;
 
                 var time = results[1]
                   , host = results[2]
@@ -61,17 +80,24 @@ window.onload = function(){
                   , content = results[4];
 
                 html = '<tr>'
-                     + '<td>' + time + '</td>'
-                     + '<td>' + host + '</td>'
-                     + '<td>' + tag + '</td>'
-                     + '<td>' + content + '</td>'
-                     + '</tr>';
+              + '<td>' + time + '</td>'
+              + '<td>' + host + '</td>'
+              + '<td>' + tag + '</td>'
+              + '<td>' + content + '</td>'
+              + '</tr>';
+                // Create and insert a tr node with record to addPoint element.
                 tempNode.innerHTML = html;
                 addPoint.appendChild(tempNode.children[0]);
             };
         };
     };
 
-    xhr.open('GET', 'system.log', true);
+    xhr.open('GET', syslogUrl, true);
     xhr.send(null);
+};
+
+window.onload = function(){
+    var addPoint = document.getElementById('add-point');
+    var syslogUrl = 'system.log';
+    showSyslog(addPoint, syslogUrl);
 };
