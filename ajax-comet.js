eb8d8750dec7cd791881a261b2338c10d89cc34f
haosdent <haosdent@gmail.com>
2013-05-22 08:00:06 +0800

    Fix scalability issues.

--- ajax-comet.js
+++ ajax-comet.js
@@ -6,6 +6,13 @@
  * @overview: Comet demo based ajax stream.
  */
 
+var records = [] // Split the complete responseText into records and save them in this array.
+  , currentPage = 1 // The current page number.
+  , recordNumLimit = 10; // The limit number of records displayed in table.
+var preBtn = document.getElementById('pre-btn')
+  , nextBtn = document.getElementById('next-btn')
+  , addPoint = document.getElementById('add-point');
+
 var getXhr = function(){
     var xhr;
     if(window.XMLHttpRequest){
@@ -24,15 +31,52 @@ var getXhr = function(){
     return xhr;
 };
 
-var appendTr = function(addPoint, html){
-    // Use for simplify dom operations.
-    var tempNode = document.createElement('tbody');
-    // Create and insert a tr node with record content to addPoint element.
-    tempNode.innerHTML = html;
-    addPoint.appendChild(tempNode.children[0]);
+// Save new responseText to global variable records.
+var saveResp = function(newResp){
+    newResp.split('\n').map(function(d){
+        records.push(d);
+    });
 };
 
-var showResp = function(addPoint, newResp){
+// Compute the number of pages, and check whether there is a next page.
+var checkNext = function(){
+    return Math.ceil(records.length / recordNumLimit) > currentPage? true : false;
+};
+
+// Check whether this is a previous page.
+var checkPre = function(){
+    return currentPage > 1? true : false;
+};
+
+// Update previous button and next button status.
+var checkBtn = function(){
+    if(checkNext()){
+        nextBtn.className = 'next';
+    }else{
+        nextBtn.className = 'next disabled';
+    };
+    if(checkPre()){
+        preBtn.className = 'previous';
+    }else{
+        preBtn.className = 'previous disabled';
+    };
+};
+
+var toNext = function(){
+    if(!checkNext()) return;
+    currentPage++;
+    checkBtn();
+    showResp();
+};
+
+var toPre = function(){
+    if(!checkPre()) return;
+    currentPage--;
+    checkBtn();
+    showResp();
+};
+
+var showResp = function(){
     /*
      * Match the informations in every syslog style record.
      * (\w+ +\d+ \d+:\d+:\d+) matches time field in record.
@@ -41,8 +85,11 @@ var showResp = function(addPoint, newResp){
      * (.+) matches content field in record.
      * */
     var regex = /(\w+ +\d+ \d+:\d+:\d+) (\S+) ([\w \]\[]+): (.+)/;
-    var data = newResp.split('\n')
-      , html;
+    var startPos = (currentPage - 1) * recordNumLimit
+      , endPos = startPos + recordNumLimit;
+    if(endPos > records.length) endPos = records.length;
+    var data = records.slice(startPos, endPos)
+      , html = '';
 
     for(var i = 0, l = data.length; i < l; i++){
         var d = data[i];
@@ -55,18 +102,17 @@ var showResp = function(addPoint, newResp){
           , tag = results[3]
           , content = results[4];
 
-        html = '<tr>'
-             + '<td>' + time + '</td>'
-             + '<td>' + host + '</td>'
-             + '<td>' + tag + '</td>'
-             + '<td>' + content + '</td>'
-             + '</tr>';
-        // Append new html to appPoint.
-        appendTr(addPoint, html);
+        html += '<tr>'
+              + '<td>' + time + '</td>'
+              + '<td>' + host + '</td>'
+              + '<td>' + tag + '</td>'
+              + '<td>' + content + '</td>'
+              + '</tr>';
     };
+    addPoint.innerHTML = html;
 };
 
-var showSyslog = function(addPoint, syslogUrl){
+var getSyslog = function(url){
     // Create ajax request object.
     var xhr = getXhr();
     // Use for mark the position of present response text.
@@ -102,9 +148,19 @@ var showSyslog = function(addPoint, syslogUrl){
         };
         // Update pos to current location.
         pos += newResp.length;
-        showResp(addPoint, newResp);
+        saveResp(newResp);
+        checkBtn();
+        showResp();
     };
 
-    xhr.open('GET', syslogUrl, true);
+    xhr.open('GET', url, true);
     xhr.send(null);
 };
+
+// Bind listener to the previous button and next button.
+preBtn.addEventListener('click', function(){
+    toPre();
+});
+nextBtn.addEventListener('click', function(){
+    toNext();
+});
